c62d863dacd8da3ab1fa7a1be0f413a63297e498
TiagoBarbosa88 <sombr43@gmail.com>
2023-05-01 13:26:12 -0300

    tela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { AnimeDetailsComponent } from './pages/anime-details/anime-details.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PostsComponent } from './pages/posts/posts.component';
 import { SidebarComponent } from './pages/sidebar/sidebar.component';
-import { AnimeDetailsComponent } from './pages/anime-details/anime-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     HomeComponent,
     PostsComponent,
     SidebarComponent,
